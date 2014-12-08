f668a47f3cc3b8cf8d7ff7cd2ff0682ecb0fbbfc
Andreas Krutzler <andi.krutzler@gmail.com>
2014-12-08 13:11:39 +0100

    Custom QSpinBox with hex in/output.
    Cause KIntSpinBox not longer exists in KDE5.

---
+++ src/klspinboxwidget.cpp
@@ -0,0 +1,6 @@
+#include "klspinboxwidget.h"
+
+KLSpinBoxWidget::KLSpinBoxWidget(QObject *parent) :
+    QSpinBox(parent)
+{
+}
---
+++ src/klspinboxwidget.h
@@ -0,0 +1,18 @@
+#ifndef KLSPINBOXWIDGET_H
+#define KLSPINBOXWIDGET_H
+
+#include <QSpinBox>
+
+class KLSpinBoxWidget : public QSpinBox
+{
+    Q_OBJECT
+public:
+    explicit KLSpinBoxWidget(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // KLSPINBOXWIDGET_H
