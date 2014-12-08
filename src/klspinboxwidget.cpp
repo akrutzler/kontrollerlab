5a2e5e9a8b68a68b4f6169be776b6c1ab8ebca14
Andreas Krutzler <andi.krutzler@gmail.com>
2014-12-08 13:20:04 +0100

    Custom QSpinBox with hex in- and output.
    Cause KIntSpinBox not longer exists in KDE5.

--- src/klspinboxwidget.cpp
+++ src/klspinboxwidget.cpp
@@ -1,6 +1,35 @@
 #include "klspinboxwidget.h"
 
-KLSpinBoxWidget::KLSpinBoxWidget(QObject *parent) :
+KLSpinBoxWidget::KLSpinBoxWidget(QWidget *parent) :
     QSpinBox(parent)
 {
+    setRange(0, 255);
+    m_validator = new QRegExpValidator(QRegExp("[0-9]{1,8}"), this);
+    m_base = 10;
 }
+
+void KLSpinBoxWidget::setValidator(QRegExpValidator *validator)
+{
+    m_validator = validator;
+}
+
+QValidator::State KLSpinBoxWidget::validate(QString &text, int &pos) const
+{
+    return m_validator->validate(text, pos);
+}
+
+int KLSpinBoxWidget::valueFromText(const QString &text) const
+{
+    return text.toInt(0, m_base);
+}
+
+QString KLSpinBoxWidget::textFromValue(int value) const
+{
+    return QString::number(value, m_base).toUpper();
+}
+
+void KLSpinBoxWidget::setBase(int base)
+{
+    m_base = base;
+}
+
--- src/klspinboxwidget.h
+++ src/klspinboxwidget.h
@@ -3,11 +3,24 @@
 
 #include <QSpinBox>
 
+class QRegExpValidator;
+
 class KLSpinBoxWidget : public QSpinBox
 {
     Q_OBJECT
 public:
-    explicit KLSpinBoxWidget(QObject *parent = 0);
+    explicit KLSpinBoxWidget(QWidget *parent = 0);
+    void setValidator(QRegExpValidator *validator);
+    void setBase(int base);
+
+protected:
+    virtual QValidator::State validate(QString &text, int &pos) const;
+    virtual int valueFromText(const QString &text) const;
+    virtual QString textFromValue(int value) const;
+
+private:
+    QRegExpValidator *m_validator;
+    int m_base;
 
 signals:
 
